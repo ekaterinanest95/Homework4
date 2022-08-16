f2cacfa3f5733d33a1f3c3b773e38a34607c6308
Ekaterina <katya.kinal@gmail.com>
2022-08-16 12:51:17 +0300

    Ex25

---
+++ Ex25/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+double result = 1;
+
+for(int i = 0; i < numberB; i++)
+{
+    result = result * numberA;
+}
+Console.WriteLine($"{numberA} -> {result}");
+
