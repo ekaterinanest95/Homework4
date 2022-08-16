1ef3595ad8c2c05d3133beb147e5605e04f1e367
Ekaterina <katya.kinal@gmail.com>
2022-08-16 14:15:10 +0300

    Ex29

---
+++ Ex29/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] GetArray(int size, int[] array)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return array;
+}
+
+int[] Array = new int[8];
+
+for(int i = 0; i < Array.Length; i++)
+{
+    Console.WriteLine($"Введите {i} элемент массива: ");
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Array = GetArray (8, Array);
+
