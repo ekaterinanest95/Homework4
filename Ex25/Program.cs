9dbc2934c66b826fc1d5d634a5c3d0b438ae70bd
Ekaterina <katya.kinal@gmail.com>
2022-08-16 13:30:26 +0300

    Ex27

--- Ex25/Program.cs
+++ Ex25/Program.cs
@@ -1,10 +1,10 @@
 ﻿int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = Convert.ToInt32(Console.ReadLine());
-double result = 1;
+int result = 1;
 
 for(int i = 0; i < numberB; i++)
 {
     result = result * numberA;
 }
-Console.WriteLine($"{numberA} -> {result}");
+Console.WriteLine($"{numberA} в степени {numberB} -> {result}");
 
---
+++ Ex27/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+int GetSumOfDigits(int num)
+{
+    int count = 0;
+    while(num % 10 > 0)
+    {
+        result = result + num % 10;
+        num = num / 10; 
+        count++;
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр в числе {number}: {GetSumOfDigits(number)}");
+
+
+
+
